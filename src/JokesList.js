33dbf171f62ddca22a3695fbc7861b0173063671
William Cao <williamcao.cmu@gmail.com>
2019-12-24 20:01:31 +0700

    first commit

---
+++ src/JokesList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class JokesList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dad Jokes</h1>
+      </div>
+    );
+  }
+}
+
+export default JokesList;
